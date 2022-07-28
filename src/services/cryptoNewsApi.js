b951f1dafcf59b5c4e56b7df35617d27edd171cd
KhalilElMhamdi <khalilelmhamdi3@gmail.com>
2022-07-28 17:14:15 +0100

    first commit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders ={
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  'X-RapidAPI-Key': 'b09026b6d7mshb2fe9ac9e22cf41p109b96jsn6396ecc8563d'
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/news"
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
